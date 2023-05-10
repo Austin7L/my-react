75882cbd48780557d402bd9c5572d842e437288c
Austin 7 Liao <austin8432@gmail.com>
2023-05-10 09:45:03 +0800

    playCss add Headers and Footer

--- pages/css.tsx
+++ pages/css.tsx
@@ -1,9 +1,13 @@
 import CssButtons from "../componets/playCss/CssButtons";
+import Headers from "../componets/index/Headers";
+import Footer from "../componets/index/Footer";
 
 const Css = () => {
   return (
     <>
+      <Headers />
       <CssButtons />
+      <Footer />
     </>
   )
 }
