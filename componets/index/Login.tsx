b0d66acc457de117c8404914adc94a8a19204600
Austin 7 Liao <austin8432@gmail.com>
2023-05-17 16:55:03 +0800

    align login input

--- componets/index/Login.tsx
+++ componets/index/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
   return (
     <>
       <form className={styles.loginForm}>
-        <label>Account: </label>
+        <label>Account : </label>
         <input type="text" className={styles.loginInput} onChange={handleId}></input>
         <br />
         <label>Password: </label>
