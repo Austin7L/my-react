15a535d740316d55fb7620db1c8ff25223514de1
Austin 7 Liao <austin8432@gmail.com>
2023-05-17 14:25:50 +0800

    update page fixed

--- componets/index/Links.tsx
+++ componets/index/Links.tsx
@@ -6,7 +6,7 @@ const Links = () => {
     { "label": "CSS", "pageName": "css", "icons": "/icons/css3.png" },
     { "label": "Unit conversion", "pageName": "unitConvert", "icons": "/icons/calculator.png" },
     { "label": "Todo List", "pageName": "todoList", "icons": "/icons/todo-list.png" },
-    { "label": "Log", "pageName": "log", "icons": "/icons/update_16px.png" },
+    { "label": "Log", "pageName": "update", "icons": "/icons/update_16px.png" },
   ];
 
   return (
